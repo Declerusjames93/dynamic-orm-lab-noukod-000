bd7045e39ef94af23042a62fa6f4634531f9ae2c
James Declerus <james.declerus@esih.edu>
2019-06-05 18:00:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,8 +2,10 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 require 'interactive_record.rb'
 
+
 class Student < InteractiveRecord
-  self.column_names.each do |col_name|
-     attr_accessor col_name.to_sym
-   end
- end
+    self.column_names.each do |column_name|
+      #turn each column name into a symbol with .to_sym
+      attr_accessor column_name.to_sym
+    end
+end
